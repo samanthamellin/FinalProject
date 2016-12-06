b4d6a10266c0b683c81aa1a26f069f2b08c22e38
Mel Mellin <mellin@uoregon.edu>
2016-12-05 17:03:10 -0800

    continued writeup and coding
    what it sounds like

--- Code.R
+++ Code.R
@@ -5,22 +5,29 @@
 setwd("C:\\Users\\smellin\\Desktop\\FinalProject")
 
 
-library(tictoc)
-library(boot)
-library(ggplot2)
-library(plotly)
+#library(tictoc)
+#library(boot)
+#library(ggplot2)
+#library(plotly)
 
-
-## this will calculate the full width 50's of the files you input
-BlackBox <- function(samp) {
-  write.csv(samp, file="resample.csv", quote=FALSE, row.names=FALSE)
-  as.numeric(shell('C:/Python34/python.exe CalcFW50.py', intern=TRUE))
-}
 
 # load the data sample
 
-two.test <- read.csv("CurrentReadings2PLCs_11_23.csv")
-three.test <- read.csv("CurrentReadings3PLCs_11_23.csv")
-four.test <- read.csv("CurrentReadings4PLCs_11_23.csv")
-five.test <- read.csv("CurrentReadings5PLCs_11_23.csv")
-six.test <- read.csv("CurrentReadings6PLCs_11_23.csv")
+# this data was all taken on the 23rd of November
+twoPLC.test <- read.csv("CurrentReadings2PLCs_11_23.csv")
+threePLC.test <- read.csv("CurrentReadings3PLCs_11_23.csv")
+fourPLC.test <- read.csv("CurrentReadings4PLCs_11_23.csv")
+fivePLC.test <- read.csv("CurrentReadings5PLCs_11_23.csv")
+sixPLC.test <- read.csv("CurrentReadings6PLCs_11_23.csv")
+# this data was taken perviously, aproximately 30 minutes
+# appart on the 15th of November
+twoA.PLC.test <- read.csv("CurrentReadings2PLC100.csv")
+threeB.PLC.test <- read.csv("CurrentReadings2PLC100num2.csv")
+
+# now we need to fix the orentation of this data
+# we want to be able to compaire areas that are
+# at the same x' value to see how noise was effected
+# across sampling rates so we will make an array
+# where each column is a new x' for each of our data
+twoPLC.currents<-array(data = NA, dim = c(100, 20))
+twoPLC.xPrime <-c()
